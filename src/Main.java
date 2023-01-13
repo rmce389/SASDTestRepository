cfb1640b33d8928c8d422a65e6317a29980ccc3f
40264792 <rmcevoy05@qub.ac.uk>
2023-01-13 10:25:11 +0000

    Removes comment

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!"); // HACK this is a bug that needs fixed
+        System.out.println("Hello World!");
 
         System.out.println("Hello 2");
-        /*
-        This is a hack multi line comment.
-        This is the second line of the hack comment.
-         */
     }
 }
